eff412550edd9055416d576d8f6993230232f0e4
leslie1964 <l84896169@gmail.com>
2025-06-28 13:56:41 +0100

    chore: jwt

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,15 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualBasic;
 
 namespace JwtAuth.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase
+public class AuthController(IConfiguration configuration) : ControllerBase
 {
     public static User user = new();
 
@@ -20,4 +23,49 @@ public class AuthController : ControllerBase
 
         return Ok(user);
     }
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+        if (user.Username != request.Username)
+        {
+            return BadRequest("Invalid Credentials");
+        }
+
+        var result = new PasswordHasher<User>().VerifyHashedPassword(
+            user,
+            user.PasswordHash,
+            request.Password
+        );
+        if (result == PasswordVerificationResult.Failed)
+        {
+            return BadRequest("Invalid Credentials");
+        }
+
+        var token = GenerateToken(user);
+        return Ok(token);
+    }
+
+    private string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+        return "GeneratedToken";
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+            audience: configuration.GetValue<string>("AppSettings:Audience"),
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds
+        );
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+
 }
