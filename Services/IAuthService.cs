9bed16a38134d74982ce087437e57bbaa56f1737
leslie1964 <l84896169@gmail.com>
2025-06-28 16:24:42 +0100

    chore: refresh tokens

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -1,6 +1,6 @@
 public interface IAuthService
 {
     Task<User?> RegisterAsync(UserDto userDto);
-    Task<string?> LoginAsync(UserDto userDto);
+    Task<TokenResponseDto?> LoginAsync(UserDto userDto);
     Task<TokenResponseDto?> RefreshTokenAsync(RefreshTokenRequestDto request); 
 }
